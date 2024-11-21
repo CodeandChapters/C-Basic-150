5e17bf9a77c5d989ae8412d21b42d8c5162a265c
Samanta Upama Oishi <samanta.u.oishi@gmail.com>
2024-11-21 12:50:47 +0600

    Add files via upload

---
+++ ex001.c
@@ -0,0 +1,15 @@
+/*Write a C program to print your name, date of birth, and mobile number.
+Expected Output:*/
+#include<stdio.h>
+int main(){
+
+printf("Name: Samanta Upama Oishi\n");
+printf("DOB: 24th September\n");
+printf("Mobile Number: 01797736941\n");
+
+return 0;
+
+
+
+
+}
