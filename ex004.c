6f96bd528d302139404b38f8238de9e0d388a1e5
Samanta Upama Oishi <samanta.u.oishi@gmail.com>
2024-11-21 17:34:40 +0600

    Update ex004.c

--- ex004.c
+++ ex004.c
@@ -8,7 +8,7 @@ char char1= 'X';
 char char2= 'M';
 char char3= 'L';
 
-printf("The reverse of %c%c%c XML is:%c%c%c",char1,char2,char3,char3,char2,char1);
+printf("The reverse of %c%c%c is:%c%c%c",char1,char2,char3,char3,char2,char1);
 
 return 0;
 
