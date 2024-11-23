03be681b654d109ae41d6dc7a2a6935bab11bfbd
Samanta Upama Oishi <samanta.u.oishi@gmail.com>
2024-11-23 10:29:17 +0600

    Add files via upload

---
+++ ex009.c
@@ -0,0 +1,27 @@
+/*Write a C program that accepts two integers from the user and calculates the sum of the two integers.*/
+
+#include<stdio.h>
+
+int main(){
+
+int num1,num2;
+
+printf("Enter The First Integar:");
+scanf("%d",&num1);
+
+printf("Enter The First Integar:");
+scanf("%d",&num2);
+
+int sum=num1+num2;
+
+printf("Sum:%d",sum);
+
+return 0;
+
+
+
+
+
+
+
+}
